699b0a59b25cdf2aac8b9a6aab715e1893d8e9fa
Yann Tavernier <yann.tavernier@graviteesource.com>
2022-05-23 14:23:36 +0200

    test: test policy with APIM Gateway SDK

---
+++ src/test/java/io/gravitee/policy/assigncontent/AssignContentPolicyIntegrationTest.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.policy.assigncontent;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.ok;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.gravitee.apim.gateway.tests.sdk.AbstractPolicyTest;
+import io.gravitee.apim.gateway.tests.sdk.annotations.DeployApi;
+import io.gravitee.apim.gateway.tests.sdk.annotations.GatewayTest;
+import io.gravitee.policy.assigncontent.configuration.AssignContentPolicyConfiguration;
+import io.reactivex.observers.TestObserver;
+import io.vertx.reactivex.core.buffer.Buffer;
+import io.vertx.reactivex.ext.web.client.HttpResponse;
+import io.vertx.reactivex.ext.web.client.WebClient;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Yann TAVERNIER (yann.tavernier at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@GatewayTest
+@DeployApi("/apis/assign-content.json")
+class AssignContentPolicyIntegrationTest extends AbstractPolicyTest<AssignContentPolicy, AssignContentPolicyConfiguration> {
+
+    @Test
+    @DisplayName("Should assign content, using Freemarker")
+    void shouldAssignContent(WebClient client) {
+        wiremock.stubFor(get("/endpoint").willReturn(ok("response from backend").withHeader("responseHeader", "responseHeaderValue")));
+
+        final TestObserver<HttpResponse<Buffer>> obs = client.get("/test").putHeader("requestHeader", "requestHeaderValue").rxSend().test();
+
+        awaitTerminalEvent(obs);
+        obs
+            .assertComplete()
+            .assertValue(response -> {
+                assertThat(response.statusCode()).isEqualTo(200);
+                assertThat(response.bodyAsString()).isEqualTo("Response body built from header 'responseHeader': responseHeaderValue");
+                return true;
+            })
+            .assertNoErrors();
+
+        wiremock.verify(
+            getRequestedFor(urlPathEqualTo("/endpoint"))
+                .withRequestBody(equalTo("Request body built from header 'requestHeader': requestHeaderValue"))
+        );
+    }
+}
